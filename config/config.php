30564674654f2702c4ecc8a79ff9bcbd47a0d50e
Jakub Theimer <jakub@dy.st>
2022-01-26 16:44:46 +0100

    add model option, document options

--- config/config.php
+++ config/config.php
@@ -1,5 +1,16 @@
 <?php
 
 return [
-    'table_prefix' => 'cookies_',
+    /**
+     * The prefix for the table names.
+     * You can use your own prefix here, eg. company_ if it were to conflict with existing table names.
+     * We leave it empty, so the table name is cookie_consents by default.
+     */
+    'table_prefix' => '',
+
+    /** 
+     * Model definition.
+     * You can extend the base model to your needs.
+     */
+    'model' => Dystcz\CookieConsentHistory\Models\CookieConsent::class,
 ];
