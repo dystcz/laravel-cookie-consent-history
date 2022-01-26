0ba2a17f27a8915a0f73663410d0c5ac5aba3346
Jakub Theimer <jakub@dy.st>
2022-01-26 20:11:29 +0100

    add example test

---
+++ tests/Unit/SaveCookieConsentTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use Dystcz\CookieConsentHistory\Actions\SaveCookieConsent;
+use Dystcz\CookieConsentHistory\Data\CookieConsentData;
+use Illuminate\Support\Str;
+
+it('can save cookie consent', function () {
+    // $action = new SaveCookieConsent();
+    //
+    // $cookie_id = Str::random();
+    // $data = [
+    //     'level' => ['necessary'],
+    //     'revision' => 0,
+    //     'data' => null,
+    //     'rfc_cookie' => false,
+    // ];
+    //
+    // $action->handle(new CookieConsentData($cookie_id, $data));
+    //
+    // $this->assertDatabaseHas('cookie_consents', [
+    //     'cookie_id' => Str::random(),
+    //     'data' => $data,
+    // ]);
+
+    $this->assertTrue(true);
+});
