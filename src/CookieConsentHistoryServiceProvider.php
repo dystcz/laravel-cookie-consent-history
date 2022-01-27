90e661e442ffa7803757c5899f27e2e1436f976b
Jakub Theimer <jakub@dy.st>
2022-01-27 12:48:37 +0100

    remove unused bindings

--- src/CookieConsentHistoryServiceProvider.php
+++ src/CookieConsentHistoryServiceProvider.php
@@ -11,36 +11,12 @@ class CookieConsentHistoryServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        /*
-         * Optional methods to load your package assets
-         */
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'laravel-cookie-consent-history');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'laravel-cookie-consent-history');
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
 
         if ($this->app->runningInConsole()) {
             $this->publishes([
                 __DIR__.'/../config/config.php' => config_path('cookie-consent-history.php'),
             ], 'config');
-
-            // Publishing the views.
-            /*$this->publishes([
-                __DIR__.'/../resources/views' => resource_path('views/vendor/laravel-cookie-consent-history'),
-            ], 'views');*/
-
-            // Publishing assets.
-            /*$this->publishes([
-                __DIR__.'/../resources/assets' => public_path('vendor/laravel-cookie-consent-history'),
-            ], 'assets');*/
-
-            // Publishing the translation files.
-            /*$this->publishes([
-                __DIR__.'/../resources/lang' => resource_path('lang/vendor/laravel-cookie-consent-history'),
-            ], 'lang');*/
-
-            // Registering package commands.
-            // $this->commands([]);
         }
     }
 
