d61d16ef2f843ac0c08b0d2d3fb0385ec149ca98
Jakub Theimer <jakub@dy.st>
2022-01-27 12:47:55 +0100

    refactor store method, remove show method

---
+++ src/Http/Controllers/CookieConsentsController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Dystcz\CookieConsentHistory\Http\Controllers;
+
+use Dystcz\CookieConsentHistory\CookieConsentHistoryFacade as CookieConsentHistory;
+use Dystcz\CookieConsentHistory\Data\CookieConsentData;
+use Dystcz\CookieConsentHistory\Http\Requests\StoreCookieConsentRequest;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+
+class CookieConsentsController extends Controller
+{
+    /**
+     * Store cookie consent data.
+     *
+     * @param StoreCookieConsentRequest $request
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(StoreCookieConsentRequest $request)
+    {
+        // Create DTO out of validated request
+        $data = new CookieConsentData(...$request->validated());
+
+        $consent = CookieConsentHistory::save($data);
+
+        return new JsonResponse([
+            'data' => $consent,
+        ]);
+    }
+}
