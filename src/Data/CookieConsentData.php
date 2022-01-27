f85c485941d202448422475746ca90cb5ae137ef
Jakub Theimer <jakub@dy.st>
2022-01-27 12:47:43 +0100

    remove cookie id

--- src/Data/CookieConsentData.php
+++ src/Data/CookieConsentData.php
@@ -5,23 +5,14 @@ namespace Dystcz\CookieConsentHistory\Data;
 class CookieConsentData
 {
     /**
-     * Cookie id.
-     *
-     * @param string $cookie_id
-     */
-    public string $cookie_id;
-
-    /**
      * Data which will be stored as JSON.
      *
      * @param array $consent_data
      */
     public array $consent_data;
 
-    public function __construct(string $cookie_id, array $consent_data)
+    public function __construct(array $consent_data)
     {
-        $this->cookie_id = $cookie_id;
-
         $this->consent_data = $consent_data;
     }
 }
