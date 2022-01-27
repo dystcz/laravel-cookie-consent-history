9cbe5b081fd62e00bbd7f74c95488661397b4456
Jakub Theimer <jakub@dy.st>
2022-01-27 12:47:36 +0100

    store just consent data

--- src/Actions/SaveCookieConsent.php
+++ src/Actions/SaveCookieConsent.php
@@ -5,15 +5,12 @@ namespace Dystcz\CookieConsentHistory\Actions;
 use Dystcz\CookieConsentHistory\Contracts\StoresCookieConsent;
 use Dystcz\CookieConsentHistory\Data\CookieConsentData;
 use Illuminate\Support\Facades\Config;
-use Illuminate\Support\Str;
 
 class SaveCookieConsent
 {
     public function handle(CookieConsentData $data): StoresCookieConsent
     {
         $consent = Config::get('cookie-consent-history.model')::create([
-            'uuid' => Str::uuid(),
-            'cookie_id' => $data->cookie_id,
             'consent_data' => $data->consent_data,
         ]);
 
